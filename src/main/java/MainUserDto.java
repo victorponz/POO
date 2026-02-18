93f4707254048aeee25bd4f482bf953fa851e78c
victorponz <victorponz@gmail.com>
2026-02-18 11:23:39 +0100

    Borrar

--- src/main/java/MainUserDto.java
+++
@@ -1,8 +0,0 @@
-record UserDto (String name, String email){
-}
-public class MainUserDto{
-    public static void main(String[] args) {
-        UserDto u = new UserDto("hola", "mundo");
-        System.out.println(u.name() + " - "  + u.email());
-    }
-}
